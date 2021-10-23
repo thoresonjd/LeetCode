58936b5e7adfac6c2a6b6d9723308d1c83b1070c
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2021-10-23 15:44:34 -0700

    Update TwoSum.py

--- Solutions/TwoSum/TwoSum.py
+++ Solutions/TwoSum/TwoSum.py
@@ -1,9 +1,26 @@
 # Justin Thoreson
 # Two Sum LeetCode solution
 
-# First attempt (best)
-# Faster than 91.52% of Python3 submissions
-# Memory less than 65.67% of Python3 submissions
+# Latest attempt (best in terms of concise code)
+# Runtime: Faster than 83.47% of Python3 online submissions for Two Sum.
+# Memory: Less than 42.38% of Python3 online submissions for Two Sum.
+# Utilization of a dictionary, similar to the hash map solution in C++
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        d = {};
+        for i, k in enumerate(nums):
+            if target-k in d:
+                return [d[target-k], i]
+            else:
+                d[k] = i
+        
+        return [-1, -1]
+    
+# Original attempt (best in terms of time and space complexity)
+# Runtime: Faster than 91.52% of Python3 submissions for Two Sum.
+# Memory: Less than 65.67% of Python3 submissions for Two Sum.
+# Require duplicate sorted array, 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
