4f65e449d92dae4889f1d763161c8fba30442a07
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2021-10-24 12:03:49 -0700

    Time: 287 ms (57.19%), Space: 46.8 MB (25.19%) - LeetHub

---
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int[] SortedSquares(int[] nums) {
+        int[] squares = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+            squares[i] = nums[i]*nums[i];
+        Array.Sort(squares);
+        return squares;
+    }
+}
