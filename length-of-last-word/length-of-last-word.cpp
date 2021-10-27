7e3f3023bb03cb2bd374c1ab74a147182e2653e2
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2021-10-26 22:13:21 -0700

    Time: 4 ms (53.77%), Space: 6.5 MB (90.09%) - LeetHub

--- length-of-last-word/length-of-last-word.cpp
+++ length-of-last-word/length-of-last-word.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int letters = 0, i = s.length(); 
-        while (s[--i] == ' ') {}                    // Traverse past whitespace
+        while (s[--i] == ' ') {}                    // Traverse through whitespace
         while (i >= 0 && s[i--] != ' ') letters++;  // Count letters in last word
         return letters;
     }
