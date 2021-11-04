960e133b2c8da2a20ca57b85f001ef92ee3f2196
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2021-11-04 12:25:15 -0700

    Time: 0 ms (100.00%), Space: 6 MB (69.31%) - LeetHub

---
+++ defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string defanged = "";
+        for (char c : address) {
+            if (c == '.')
+                defanged += "[.]";
+            else
+                defanged += c;
+        }
+        return defanged;
+    }
+};
