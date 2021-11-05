df7c2f0d201958669424420f8867079ad8f38c5a
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2021-11-04 20:23:41 -0700

    Time: 8 ms (50.75%), Space: 9.8 MB (24.76%) - LeetHub

---
+++ shuffle-the-array/shuffle-the-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> res;
+        vector<int> left(nums.begin(), nums.end() - n);
+        vector<int> right(nums.begin() + n, nums.end());
+        for (int i = 0; i < n; i++) {
+            res.push_back(left[i]);
+            res.push_back(right[i]);
+        }
+        return res;
+    }
+};
