d9c9f3fa19f927cb2a2a2cca38dbb7b64ed1d09b
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2021-11-04 20:27:15 -0700

    Time: 8 ms (50.75%), Space: 9.8 MB (57.86%) - LeetHub

--- shuffle-the-array/shuffle-the-array.cpp
+++ shuffle-the-array/shuffle-the-array.cpp
@@ -2,12 +2,18 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> res;
-        vector<int> left(nums.begin(), nums.end() - n);
-        vector<int> right(nums.begin() + n, nums.end());
         for (int i = 0; i < n; i++) {
-            res.push_back(left[i]);
-            res.push_back(right[i]);
+            res.push_back(nums[i]);
+            res.push_back(nums[i+n]);
         }
         return res;
+        // vector<int> res;
+        // vector<int> left(nums.begin(), nums.end() - n);
+        // vector<int> right(nums.begin() + n, nums.end());
+        // for (int i = 0; i < n; i++) {
+        //     res.push_back(left[i]);
+        //     res.push_back(right[i]);
+        // }
+        // return res;
     }
 };
