1545650acf4464e40e5bbb9104c037cacf883139
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2021-11-09 09:41:21 -0800

    Time: 4 ms (41.36%), Space: 6 MB (46.54%) - LeetHub

--- number-of-1-bits/number-of-1-bits.cpp
+++ number-of-1-bits/number-of-1-bits.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int hamWt = 0;
-        while (n > 0) {
+        while (n > 0) {     // No more 1 bits when n is zero
             hamWt += n & 1; // Check rightmost bit, add to weight if 1
             n = n >> 1;     // Shift bits to the right
         }
