d62957466b14358df9ae4a2bdcc4b954510f3c93
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2021-11-15 22:53:12 -0800

    Time: 8 ms (95.12%), Space: 11.2 MB (18.26%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,6 +1,29 @@
 // Justin Thoreson
 // Two Sum Leetcode Solution
 
+// Latest C++ attempt
+// Runtime: Faster than 95.35% of C++ online submissions for Two Sum.
+// Memory: Less than 24.97% of C++ online submissions for Two Sum.
+// Attended a hacking lab on hash maps, solution was learned via group discussion.
+
+// If a number in the vector does not exist in the map, 
+// add it as a key with the index as its value.
+// Otherwise, if there exists a key k such that the current number + k = target,
+// return the index of k and the index of the current number
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> hash;
+        for (int i = 0; i < nums.size(); i++) {
+            if (hash.find(target-nums[i]) != hash.end())
+                return {hash[target-nums[i]], i};
+            else
+                hash[nums[i]] = i;
+        }
+        return {-1, -1};
+    }
+};
+
 // Original Solution:
 // Time: 8 ms (95.35%)
 // Space: 10.3 MB (62.43%) - LeetHub
@@ -25,30 +48,6 @@ public:
             }
         }
         
-        return {-1, -1};
-    }
-};
-
-// Latest C++ attempt
-// Runtime: Faster than 95.35% of C++ online submissions for Two Sum.
-// Memory: Less than 24.97% of C++ online submissions for Two Sum.
-// Attended a hacking lab on hash maps, solution was learned via group discussion.
-
-// If a number in the vector does not exist in the map, 
-// add it as a key with the index as its value.
-// Otherwise, if there exists a key k such that the current number + k = target,
-// return the index of k and the index of the current number
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> hash;
-        vector<int> res;
-        for (int i = 0; i < nums.size(); i++) {
-            if (hash.find(target-nums[i]) != hash.end())
-                return {hash[target-nums[i]], i};
-            else
-                hash[nums[i]] = i;
-        }
         return {-1, -1};
     }
 };
