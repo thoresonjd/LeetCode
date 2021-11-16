35f2e03346b980fb3e24ee98718c5c30198a75be
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2021-11-15 23:34:33 -0800

    Time: 80 ms (83.66%), Space: 58.9 MB (80.66%) - LeetHub

--- container-with-most-water/container-with-most-water.cpp
+++ container-with-most-water/container-with-most-water.cpp
@@ -11,3 +11,27 @@ public:
         return water;
     }
 };
+
+// Calulate area of widest container.
+// Inside containers are less wide,
+// so only compute new area 
+// if height of new wall is 
+// greater than old min height
+
+// Below only works if the tallest height is always in the answer
+//         pair<int, int> tallest = {INT_MIN, INT_MIN};
+//         for (int i = 0; i < height.size(); i++) {
+//             if (height[i] > tallest.second) {
+//                 tallest.first = i;
+//                 tallest.second = height[i];
+//             }
+//         }
+//        
+//         int maxArea = 0, area;
+//         for (int i = 0; i < height.size(); i++) {
+//             area = height[i] * (tallest.first > i ? tallest.first - i : i - tallest.first);
+//             if (area > maxArea)
+//                 maxArea = area;
+//         }
+//      
+//         return maxArea;
