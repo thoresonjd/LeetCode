ddc414a649d205b3443363e2ed453e5c0d51e3c5
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2021-11-17 14:42:04 -0800

    Time: 68 ms (88.49%), Space: 37.1 MB (50.89%) - LeetHub

---
+++ subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> m = {{0,1}};
+        int count = 0, sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            if(m.find(sum-k) != m.end())
+                count += m[sum-k];
+            m[sum]++;
+        }
+        return count;
+    }
+};
