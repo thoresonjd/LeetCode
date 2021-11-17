c84b095450806afbbcfd3d76be35bbba4b89e8b5
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2021-11-17 14:55:03 -0800

    Time: 20 ms (84.61%), Space: 31.2 MB (28.26%) - LeetHub

---
+++ find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum = 0, total = accumulate(nums.begin(), nums.end(), 0);
+        for (int i = 0; i < nums.size(); i++) {
+            if (sum * 2 == total - nums[i])
+                return i;
+            sum += nums[i];    
+        }
+        return -1;
+    }
+};
