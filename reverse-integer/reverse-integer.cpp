7ad527841e12c9d3f3125b2705285272cc7ae746
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2021-11-18 11:47:51 -0800

    Time: 4 ms (40.6%), Space: 6.5 MB (5.06%) - LeetHub

--- reverse-integer/reverse-integer.cpp
+++ reverse-integer/reverse-integer.cpp
@@ -1,3 +1,26 @@
+// Latest solution
+class Solution {
+public:
+    int reverse(int x) {
+        if (x >= INT_MAX || x <= INT_MIN) return 0;
+        
+        string num = to_string(abs(x));
+        int sign = x > 0 ? 1 : -1;
+        
+        for (int i = 0 ; i < num.length()/2; i++)
+            swap(num[i], num[num.length()-i-1]);
+        
+        try {
+            return sign * stoi(num);
+        } catch (out_of_range& e) {
+            printf("Out of range");
+        }
+        
+        return 0;
+    }
+};
+
+// Old solution
 class Solution {
 public:
     int reverse(int x) {
