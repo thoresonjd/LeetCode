e74a3d6d326af91c648bc341f407e274074a200d
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2022-01-01 14:52:04 -0800

    Time: 64 ms (98.42%), Space: 59.1 MB (36.92%) - LeetHub

--- container-with-most-water/container-with-most-water.cpp
+++ container-with-most-water/container-with-most-water.cpp
@@ -1,37 +1,13 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int water = INT_MIN, l = 0, r = height.size() - 1;
+        int water = 0, l = 0, r = height.size() - 1;
         while (l < r) {
             int h = min(height[l], height[r]);
-            water = max(water, (r - l) * h);
-            while (height[r] <= h && l < r) r--;
+            water = max(water, h*(r-l));
             while (height[l] <= h && l < r) l++;
+            while (height[r] <= h && l < r) r--;
         }
         return water;
     }
 };
-
-// Calulate area of widest container.
-// Inside containers are less wide,
-// so only compute new area 
-// if height of new wall is 
-// greater than old min height
-
-// Below only works if the tallest height is always in the answer
-//         pair<int, int> tallest = {INT_MIN, INT_MIN};
-//         for (int i = 0; i < height.size(); i++) {
-//             if (height[i] > tallest.second) {
-//                 tallest.first = i;
-//                 tallest.second = height[i];
-//             }
-//         }
-//        
-//         int maxArea = 0, area;
-//         for (int i = 0; i < height.size(); i++) {
-//             area = height[i] * (tallest.first > i ? tallest.first - i : i - tallest.first);
-//             if (area > maxArea)
-//                 maxArea = area;
-//         }
-//      
-//         return maxArea;
