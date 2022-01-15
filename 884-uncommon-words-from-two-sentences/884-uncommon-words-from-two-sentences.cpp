a05f2f0449df0ec6b63f9a4d7304a8a904189732
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2022-01-15 13:13:41 -0800

    Time: 0 ms (100.00%), Space: 6.9 MB (51.69%) - LeetHub

--- 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.cpp
+++ 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.cpp
@@ -4,28 +4,26 @@ class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
         unordered_map<string, int> freq;
-        
+        vector<string> res;
         istringstream stream1(s1);
+        istringstream stream2(s2);
+        
         do {
             string word;
             stream1 >> word;
             freq[word]++;
         } while (stream1);
         
-        istringstream stream2(s2);
         do {
             string word;
             stream2 >> word;
             freq[word]++;
         } while (stream2);
         
-        vector<string> res;
-        
         for (auto it = freq.begin(); it != freq.end(); it++) {
             if (it->second == 1)
                 res.push_back(it->first);
         }
-        
         
         return res;
     }
