9426a1ac464cb5b366b85dbcb125e7b00e7aec33
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2022-01-29 10:02:14 -0800

    Time: 0 ms (100.00%), Space: 6.8 MB (64.84%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.cpp
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxPower(string s) {
-        if (!s.length()) return 0;
+        // if (!s.length()) return 0;
         int power = 1, maxPower = 1;
         for (int i = 1; i < s.length(); i++) {
             if (s[i-1] == s[i]) power++;
