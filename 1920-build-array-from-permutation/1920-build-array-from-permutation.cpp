c552c6fba824fb6ce910b96a52adcb3fa9747b55
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2022-02-01 15:58:45 -0800

    Time: 24 ms (33.02%), Space: 16.1 MB (99.11%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        vector<int> ans;
-        for (int num : nums)
-            ans.push_back(nums[num]);
+        vector<int> ans(nums.size());
+        for (int i = 0; i < nums.size(); i++)
+            ans[i] = nums[nums[i]];
         return ans;
+        
+        // vector<int> ans;
+        // for (int num : nums)
+        //     ans.push_back(nums[num]);
+        // return ans;
     }
 };
