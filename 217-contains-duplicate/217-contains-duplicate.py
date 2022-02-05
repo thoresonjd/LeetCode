a17625e54d1ce3437b161b585ba3bc0a59f5f9a1
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2022-02-04 17:42:37 -0800

    Time: 519 ms (49.16%), Space: 26.1 MB (26.82%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,4 +1,3 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
         return len(set(nums)) < len(nums)
-        
