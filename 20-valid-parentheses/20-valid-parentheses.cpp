65ad9d5d8d273eaf4ecc956825f62f803c6f8272
Justin Thoreson <77815463+thoresonjd@users.noreply.github.com>
2022-03-20 21:09:51 -0700

    Time: 0 ms (100.00%), Space: 6.3 MB (51.31%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[')
+                stk.push(c);
+            else if (stk.empty())
+                return false;
+            else if ((stk.top() == '(' && c == ')') || 
+                     (stk.top() == '{' && c == '}') || 
+                     (stk.top() == '[' && c == ']'))
+                stk.pop();
+            else 
+                return false;
+        }
+        return stk.empty();
+    }
+};
